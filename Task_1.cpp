5a7fb5bacc81b27a4173721aec677201d4fbe958
Al Rubayet Turjo <rubayet125@gmail.com>
2024-09-18 11:23:39 +0600

    Fixing Bug And Adding .exe file

--- Task_1.cpp
+++ Task_1.cpp
@@ -67,7 +67,7 @@ public:
 
     void accessATM(string accountHolder, string pin) {
         if (accounts.find(accountHolder) != accounts.end()) {
-            Account &account = accounts[accountHolder];
+            Account account = accounts[accountHolder];
             if (account.validatePIN(pin)) {
                 int choice;
                 do {
@@ -77,6 +77,7 @@ public:
                     cout << "3. Withdraw" << endl;
                     cout << "4. Change PIN" << endl;
                     cout << "5. Exit" << endl;
+                    cout<<endl;
                     cout << "Enter your choice: ";
                     cin >> choice;
 
@@ -87,23 +88,46 @@ public:
                         }
                         case 2: {
                             double depositAmount;
+                            string pin;
                             cout << "Enter deposit amount: ";
                             cin >> depositAmount;
-                            account.deposit(depositAmount);
+                            cout<<"Confirm Your PIN :";
+                            cin>>pin;
+                            if(account.validatePIN(pin)){
+                                account.deposit(depositAmount);
+                            }else{
+                                cout<<"Wrong PIN. Please try Again."<<endl;
+                            }
+                            
                             break;
                         }
                         case 3: {
                             double withdrawAmount;
+                            string pin;
                             cout << "Enter withdrawal amount: ";
                             cin >> withdrawAmount;
-                            account.withdraw(withdrawAmount);
+                            cout<<"Confirm Your PIN :";
+                            cin>>pin;
+                            if(account.validatePIN(pin)){
+                                account.withdraw(withdrawAmount);
+                            }else{
+                                cout<<"Wrong PIN. Please try Again."<<endl;
+                            }
+                            
                             break;
                         }
                         case 4: {
-                            string newPIN;
-                            cout << "Enter new PIN: ";
-                            cin >> newPIN;
-                            account.changePIN(newPIN);
+                            string currPIN,newPIN;
+                            cout<<"Enter Current PIN: ";
+                            cin >>currPIN;
+                            if(account.validatePIN(currPIN)){
+                                cout << "Enter new PIN: ";
+                                cin >> newPIN;
+                                account.changePIN(newPIN);
+                            }else{
+                                cout<<"Wrong PIN. Please try Again."<<endl;
+                            }
+                            
                             break;
                         }
                         case 5: {
@@ -129,15 +153,14 @@ int main() {
 
     ATM atm;
     //we can add as many account as we want
-    atm.addAccount("John Doe", "1234", 1000.0);
+    atm.addAccount("Al Rubayet", "1234", 1000.0);
     atm.addAccount("Jane Smith", "4321", 1500.0);
 
     string accountHolder, pin;
 
     cout << "Welcome to the ATM!" << endl;
     cout << "Enter account holder name: ";
-    cin >> accountHolder;
-
+    getline(cin, accountHolder);
     cout << "Enter PIN: ";
     cin >> pin;
 
